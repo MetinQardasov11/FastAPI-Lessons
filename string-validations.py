95ed43d8a82f5a5290d1893234208a24ccc4c291
MetinQardasov11 <metin.qardasov2003@gmail.com>
2025-10-14 11:39:05 +0400

    Day 02

--- string-validations.py
+++ string-validations.py
@@ -5,138 +5,138 @@ from typing import Annotated
 app = FastAPI()
 
 
-# @app.get("/items/")
-# async def read_items(q: Annotated[str | None, Query(max_length=50)] = None):
-#     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-#     if q:
-#         results.update({"q": q})
-#     return results
+@app.get("/items/")
+async def read_items(q: Annotated[str | None, Query(max_length=50)] = None):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
 
 
 # ! or
 
-# @app.get("/items/")
-# async def read_items(q: str | None = Query(default=None, max_length=50)):
-#     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-#     if q:
-#         results.update({"q": q})
-#     return results
+@app.get("/items/")
+async def read_items(q: str | None = Query(default=None, max_length=50)):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
 
 
 
-# @app.get("/items/")
-# async def read_items(q: str | None = Query(default="Metin", max_length=50)):
-#     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-#     if q:
-#         results.update({"q": q})
-#     return results
+@app.get("/items/")
+async def read_items(q: str | None = Query(default="Metin", max_length=50)):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
 
 
-# @app.get("/items/")
-# async def read_items(q: Annotated[str | None, Query(min_length=3, max_length=50)] = None):
-#     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-#     if q:
-#         results.update({"q": q})
-#     return results
+@app.get("/items/")
+async def read_items(q: Annotated[str | None, Query(min_length=3, max_length=50)] = None):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
 
 
 
-# @app.get("/items/")
-# async def read_items(q: Annotated[str | None, Query(min_length=3, max_length=50, regex="^some$")] = None,):
+@app.get("/items/")
+async def read_items(q: Annotated[str | None, Query(min_length=3, max_length=50, regex="^some$")] = None,):
     
-#     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-#     if q:
-#         results.update({"q": q})
-#     return results
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
 
 
-# @app.get("/items/")
-# async def read_items(q: Annotated[str, Query(min_length=3)] = "fixedquery"):
-#     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-#     if q:
-#         results.update({"q": q})
-#     return results
+@app.get("/items/")
+async def read_items(q: Annotated[str, Query(min_length=3)] = "fixedquery"):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
 
 
-# @app.get("/items/")
-# async def read_items(q: Annotated[str, Query(min_length=10, max_length=50)] = "fixedquery"):
-#     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-#     if q:
-#         results.update({"q": q})
-#     return results
+@app.get("/items/")
+async def read_items(q: Annotated[str, Query(min_length=10, max_length=50)] = "fixedquery"):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
 
 
-# @app.get("/items/")
-# async def read_items(q: Annotated[list[str] | None, Query()] = None):
-#     query_items = {"q": q}
-#     return query_items
+@app.get("/items/")
+async def read_items(q: Annotated[list[str] | None, Query()] = None):
+    query_items = {"q": q}
+    return query_items
 
 # ! or
 
-# @app.get("/items/")
-# async def read_items(q: Annotated[list, Query()] = []):
-#     query_items = {"q": q}
-#     return query_items
+@app.get("/items/")
+async def read_items(q: Annotated[list, Query()] = []):
+    query_items = {"q": q}
+    return query_items
 
 
-# @app.get("/items/")
-# async def read_items(q: Annotated[list[str], Query()] = ["foo", "bar"]):
-#     query_items = {"q": q}
-#     return query_items
+@app.get("/items/")
+async def read_items(q: Annotated[list[str], Query()] = ["foo", "bar"]):
+    query_items = {"q": q}
+    return query_items
 
 
-# @app.get("/items/")
-# async def read_items(q: Annotated[str | None, Query(title="Query string", min_length=3)] = None):
-#     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-#     if q:
-#         results.update({"q": q})
-#     return results
+@app.get("/items/")
+async def read_items(q: Annotated[str | None, Query(title="Query string", min_length=3)] = None):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
 
 
-# @app.get("/items/")
-# async def read_items(
-#     q: Annotated[
-#         str | None,
-#         Query(
-#             title="Query string",
-#             description="Query string for the items to search in the database that have a good match",
-#             min_length=3,
-#         ),
-#     ] = None,
-# ):
-#     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-#     if q:
-#         results.update({"q": q})
-#     return results
+@app.get("/items/")
+async def read_items(
+    q: Annotated[
+        str | None,
+        Query(
+            title="Query string",
+            description="Query string for the items to search in the database that have a good match",
+            min_length=3,
+        ),
+    ] = None,
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
 
 
-# @app.get("/items/")
-# async def read_items(q: Annotated[str | None, Query(alias="item-query")] = None):
-#     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-#     if q:
-#         results.update({"q": q})
-#     return results
+@app.get("/items/")
+async def read_items(q: Annotated[str | None, Query(alias="item-query")] = None):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
 
 
-# @app.get("/items/")
-# async def read_items(
-#     q: Annotated[
-#         str | None,
-#         Query(
-#             alias="item-query",
-#             title="Query string",
-#             description="Query string for the items to search in the database that have a good match",
-#             min_length=3,
-#             max_length=50,
-#             pattern="^fixedquery$",
-#             deprecated=True,
-#         ),
-#     ] = None,
-# ):
-#     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-#     if q:
-#         results.update({"q": q})
-#     return results
+@app.get("/items/")
+async def read_items(
+    q: Annotated[
+        str | None,
+        Query(
+            alias="item-query",
+            title="Query string",
+            description="Query string for the items to search in the database that have a good match",
+            min_length=3,
+            max_length=50,
+            pattern="^fixedquery$",
+            deprecated=True,
+        ),
+    ] = None,
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
 
 
 @app.get("/items/")
